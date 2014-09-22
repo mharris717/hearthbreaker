179b57caacff2d08446d5319520830d831bb32dd
Mike Harris <mharris717@gmail.com>
2014-09-22 12:51:34 -0400

    lint fixes

--- hearthbreaker/agents/trade_agent.py
+++ hearthbreaker/agents/trade_agent.py
@@ -1,70 +1,43 @@
 from hearthbreaker.agents.basic_agents import RandomAgent
-from hearthbreaker.agents.trade.possible_play import PossiblePlays,PlayMixin
-from hearthbreaker.agents.trade.trade import Trade,TradeMixin,AttackMixin
-from hearthbreaker.game_objects import Hero
+from hearthbreaker.agents.trade.possible_play import PlayMixin
+from hearthbreaker.agents.trade.trade import TradeMixin, AttackMixin
 from hearthbreaker.agents.trade.util import Util
 
-class ChooseTargetMixin:
-    def gdfgdfggdfgdf(self):
-        print('hello')
 
+class ChooseTargetMixin:
     def choose_target_enemy(self, targets):
         if len(targets) == 0:
             raise Exception("No targets")
 
         if not self.current_trade:
             return Util.rand_el(targets)
-            #raise Exception("No current trade")
+            # raise Exception("No current trade")
 
         for target in targets:
             if self.current_trade.opp_minion == target:
                 return target
 
-        #raise Exception("Could not find target {}".format(target))
+        # raise Exception("Could not find target {}".format(target))
         return Util.rand_el(targets)
 
-    def choose_target_friendly(self,targets):
+    def choose_target_friendly(self, targets):
         return targets[0]
 
-    def target_owner(self,player,target):
-        for minion in player.minions:
-            if minion == target: return "Me"
-        for minion in player.opponent.minions:
-            if minion == target: return "Opponent"
-
-        if target.__class__ == Hero: return "Hero"
-
-        raise Exception("No Owner of {}".format(target.name))
-
-
-    def choose_target(self,targets):
-        if len(targets) == 0: return None
-        owners = [self.target_owner(self.player,target) for target in targets]
-        # print(owners)
+    def choose_target(self, targets):
+        if len(targets) == 0:
+            return None
         return self.choose_target_enemy(targets)
 
-class TradeAgent(TradeMixin,AttackMixin,PlayMixin,ChooseTargetMixin,RandomAgent):
+
+class TradeAgent(TradeMixin, AttackMixin, PlayMixin, ChooseTargetMixin, RandomAgent):
     def __init__(self):
         super().__init__()
         self.current_trade = None
 
-    def playable_cards(self, player):
-        raise Exception("unused")
-        res = filter(lambda card: card.can_use(player, player.game), player.hand)
-        return Util.reverse_sorted(res,Card.mana)
-
     def do_turn(self, player):
-        #raise Exception("do_turn")
-        #print("do_turn")
         self.player = player
         self.play_cards(player)
         self.attack(player)
 
         if not player.game.game_ended:
-          self.play_cards(player)
-
-    
-
-    
-
-
+            self.play_cards(player)
--- tests/agents/trade/test_helpers.py
+++ tests/agents/trade/test_helpers.py
@@ -1,49 +1,49 @@
 import random
-import unittest
 from hearthbreaker.agents.basic_agents import RandomAgent
 from hearthbreaker.agents.trade_agent import TradeAgent
-from hearthbreaker.cards import GoldshireFootman, MurlocRaider, BloodfenRaptor, FrostwolfGrunt, RiverCrocolisk, \
-    IronfurGrizzly, MagmaRager, SilverbackPatriarch, ChillwindYeti, SenjinShieldmasta, BootyBayBodyguard, \
-    FenCreeper, BoulderfistOgre, WarGolem, Shieldbearer, FlameImp, YoungPriestess, DarkIronDwarf, DireWolfAlpha, \
-    VoidWalker, HarvestGolem, KnifeJuggler, ShatteredSunCleric, ArgentSquire, Doomguard, Soulfire, DefenderOfArgus, \
-    AbusiveSergeant, NerubianEgg, KeeperOfTheGrove
+from hearthbreaker.cards import WarGolem
 from hearthbreaker.constants import CHARACTER_CLASS
-from hearthbreaker.game_objects import Deck, Game, MinionCard, Minion, TheCoin, Player, Bindable
+from hearthbreaker.game_objects import Deck, Game, MinionCard, Minion, TheCoin, Player
 import re
+
 
 def t(self):
     return self.name
 Minion.try_name = t
 
+
 class TempCard:
-    def __init__(self,base_attack,health,name="",taunt=False):
+    def __init__(self, base_attack, health, name="", taunt=False):
         self.base_attack = base_attack
         self.health = health
         self.name = name
         self.taunt = taunt
         self.mana = None
 
-    def create_minion(self,player):
-        res = Minion(self.base_attack,self.health,taunt=self.taunt)
+    def create_minion(self, player):
+        res = Minion(self.base_attack, self.health, taunt=self.taunt)
         res.name = self.name
         return res
 
     @staticmethod
     def make(s):
         taunt = False
-        a,h = s.split("/")
-        g = re.search("(\d+)t$",h)
+        a, h = s.split("/")
+        g = re.search("(\d+)t$", h)
         if g:
             taunt = True
             h = g.group(1)
-        return TempCard(int(a),int(h),taunt=taunt,name=s)
+        return TempCard(int(a), int(h), taunt=taunt, name=s)
+
 
 class FakePlayer(Player):
-    def draw(self): None
+    def draw(self):
+        None
+
 
 class FakeGame(Game):
     def __init__(self, decks, agents, random_func=random.randint):
-        super(Game,self).__init__()
+        super(Game, self).__init__()
         self.delayed_minions = set()
         self.random = random_func
         first_player = random_func(0, 1)
@@ -65,29 +65,33 @@ class FakeGame(Game):
         for i in range(0, 4):
             self.players[1].draw()
 
+
 class TestHelpers:
     @staticmethod
     def fix_create_minion(classes=None):
-        if not classes: classes = MinionCard.__subclasses__()
+        if not classes:
+            classes = MinionCard.__subclasses__()
         for cls in classes:
             TestHelpers.fix_create_minion_single(cls)
 
     @staticmethod
     def fix_create_minion_single(cls):
-        if isinstance(cls,str): sdfsdfd()
-        if isinstance(cls,TheCoin): return
+        if isinstance(cls, TheCoin):
+            return
 
-        if not hasattr(cls,"create_minion_old"):
+        if not hasattr(cls, "create_minion_old"):
             old = cls.create_minion
             cls.create_minion_old = old
-            def create_minion_named_gen(self,player):
-                res = old(self,player)
+
+            def create_minion_named_gen(self, player):
+                res = old(self, player)
                 res.name = self.name
                 return res
+
             cls.create_minion = create_minion_named_gen
 
-    def make_game(self,before_draw_callback=None):
-        cs = [WarGolem() for i in range(0,30)]
+    def make_game(self, before_draw_callback=None):
+        cs = [WarGolem() for i in range(0, 30)]
         deck1 = Deck(cs.copy(), CHARACTER_CLASS.DRUID)
         deck2 = Deck(cs.copy(), CHARACTER_CLASS.WARLOCK)
 
